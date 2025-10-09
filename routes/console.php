53cfb59970c500533ccc9ec366b18629906a3525
IlyaKondryukov <ilyakondryukov8765@gmail.com>
2025-10-10 00:11:37 +0300

    генерация sitemap каждую ночь

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Генерация sitemap каждую ночь в 3:00
+Schedule::command('sitemap:generate')->dailyAt('03:00');
