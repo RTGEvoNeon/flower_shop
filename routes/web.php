4435cdbbdce659a72ae9e2c50972c2475ac2eeb3
IlyaKondryukov <ilyakondryukov8765@gmail.com>
2025-12-11 01:52:18 +0300

    Упростил проект: удалил корзину и лишние view

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,23 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
+// Главная страница
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/product/{id}', [ProductController::class, 'show']);
+// Каталог товаров
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/product/{slug}', [ProductController::class, 'show'])->name('products.show');
 
-// Custom bouquet
+// Форма заказа кастомного букета
 Route::get('/custom-bouquet', function () {
     return view('custom-bouquet');
-});
-Route::post('/custom-bouquet/submit', function () {
-    // Пока просто редирект с сообщением
-    return redirect('/custom-bouquet')->with('success', 'Спасибо! Мы скоро с вами свяжемся!');
-});
+})->name('custom-bouquet.show');
 
-// Cart routes
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
-Route::post('/cart/update', [CartController::class, 'update'])->name('cart.update');
-Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
-Route::get('/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
-Route::post('/order', [CartController::class, 'placeOrder'])->name('order.place');
-Route::get('/order/success/{id}', [CartController::class, 'orderSuccess'])->name('order.success');
+Route::post('/custom-bouquet/submit', function () {
+    // TODO: Добавить отправку в Telegram
+    return redirect('/custom-bouquet')->with('success', 'Спасибо! Мы скоро с вами свяжемся!');
+})->name('custom-bouquet.submit');
